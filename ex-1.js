d2259739e7b6bad56f4e9d540fe1a842236b0d80
Emmiiez <emmiiez.m@gmail.com>
2024-06-12 18:54:08 +0700

    Update ex-1.js
    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,7 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
 
-// Start coding here
+for (i = 0 ; i < score.length ; i++ ) {
+    score[i] -= 10;
+}
+console.log(score);
