fb14f52ea6c1700d91f5c482a87cdbdedbaf5509
Emmiiez <emmiiez.m@gmail.com>
2024-06-12 18:55:45 +0700

    Update ex-2.js
    Update ex-2.js

--- ex-2.js
+++ ex-2.js
@@ -1,6 +1,8 @@
 // Exercise #2: Sum the Numbers
-let numbers = [10, 20, 30, 40, 50];
-let total = 0;
-// Start coding here
+let productPrice = [10, 20, 30, 40, 50];
+sum = 0 ;
 
-console.log(total);
+for (let i = 0 ; i < productPrice.length ; i++) {
+    sum += productPrice[i];
+}
+console.log(sum);
