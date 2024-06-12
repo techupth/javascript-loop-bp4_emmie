b4652d87dfa8dfbe14b4693b353611bbff4ce83f
Emmiiez <emmiiez.m@gmail.com>
2024-06-12 21:21:29 +0700

    Update ex-4.js

--- ex-4.js
+++ ex-4.js
@@ -1,8 +1,10 @@
 // Exercise #4: Displaying Restaurant Name
 let restaurantName = ["Restaurant Name: KFC", "Restaurant Name: MOMO PARADISE", "Restaurant Name: Nabezo"];
-let i = 0;
+let newRes = [];
 
-while (i < restaurantName.length){
-    console.log("Restaurant Name: " + restaurantName[i]);
-    i++;
+let i = 0;
+while (i < restaurantName.length) {
+    newRes.push(`Restaurant Name: ${restaurantName[i]}`);
+i++;
 }
+console.log(newRes);
